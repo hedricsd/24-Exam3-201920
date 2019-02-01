242e49a20b05fe4b6c322721e60f7d4f93108609
hedricsd <hedricsd@rose-hulman.edu>
2019-01-31 21:15:09 -0500

    Done

--- src/problem1.py
+++ src/problem1.py
@@ -3,9 +3,9 @@ Exam 3, problem 1.
 
 Authors: Vibha Alangar, Aaron Wilkin, David Mutchler, Dave Fisher, 
          Matt Boutell, Amanda Stouder, their colleagues and 
-         PUT_YOUR_NAME_HERE.  January 2019.
+         Sam Hedrick.  January 2019.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -126,7 +126,7 @@ def problem1(n):
       :rtype: int
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # **** IMPORTANT ****:
     # We supplied a   fibonacci   function above.
@@ -139,7 +139,15 @@ def problem1(n):
     # and THEN try the full-credit version.
     # As always, CONTINUE to the next problem if you are STUCK on this one.
     # -------------------------------------------------------------------------
-
+    #for k in range(1, n + 1):
+    #   if fibonacci(k) + fibonacci(k + 1) >= n:
+    #       return fibonacci(k +1)
+    fib = [0, 1]
+    for k in range(n):
+        fib.append(fib[k] + fib[k+1])
+    for j in range(len(fib)):
+        if fib[j + 1] >= n:
+            return fib[j]
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
